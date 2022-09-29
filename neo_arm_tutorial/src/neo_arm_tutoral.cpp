dea7fc48cd3851cb9b317b0c1fc73185be2472b2
Pradheep <padhupradheep@gmail.com>
2022-09-29 11:40:44 +0200

    adding tutorial with reachable cartesian poses from the home position

---
+++ neo_arm_tutorial/src/neo_arm_tutoral.cpp
@@ -0,0 +1,75 @@
+#include <memory>
+
+#include <rclcpp/rclcpp.hpp>
+#include <moveit/move_group_interface/move_group_interface.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
+
+int main(int argc, char * argv[])
+{
+  // Initialize ROS and create the Node
+  rclcpp::init(argc, argv);
+  auto const node = std::make_shared<rclcpp::Node>(
+    "neo_arm_tutorial",
+    rclcpp::NodeOptions().automatically_declare_parameters_from_overrides(true)
+  );
+
+  rclcpp::executors::SingleThreadedExecutor executor;
+  executor.add_node(node);
+  std::thread([&executor]() { executor.spin(); }).detach();
+
+  // Create a ROS logger
+  auto const logger = rclcpp::get_logger("neo_arm_tutorial");
+
+  static const std::string PLANNING_GROUP = "arm";
+
+  moveit::planning_interface::MoveGroupInterface move_group(node, PLANNING_GROUP);
+
+  const moveit::core::JointModelGroup* joint_model_group =
+      move_group.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
+
+  moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
+
+  moveit::core::RobotState start_state(*move_group.getCurrentState());
+  geometry_msgs::msg::Pose start_pose2;
+  start_pose2.orientation.w = 1.0;
+  start_pose2.position.x = -0.1;
+  start_pose2.position.y = -0.01;
+  start_pose2.position.z = -0.1;
+  start_state.setFromIK(joint_model_group, start_pose2);
+  move_group.setStartState(start_state);
+
+  std::vector<geometry_msgs::msg::Pose> waypoints;
+  waypoints.push_back(start_pose2);
+
+  geometry_msgs::msg::Pose target_pose3 = start_pose2;
+
+  target_pose3.position.x -= 0.2;
+  waypoints.push_back(target_pose3);  // down
+
+  target_pose3.position.y -= 0.2;
+  // waypoints.push_back(target_pose3);  // right
+
+  target_pose3.position.z += 0.2;
+  target_pose3.position.y += 0.2;
+  target_pose3.position.x -= 0.2;
+  // waypoints.push_back(target_pose3);  // up and left
+
+  // We want the Cartesian path to be interpolated at a resolution of 1 cm
+  // which is why we will specify 0.01 as the max step in Cartesian
+  // translation.  We will specify the jump threshold as 0.0, effectively disabling it.
+  // Warning - disabling the jump threshold while operating real hardware can cause
+  // large unpredictable motions of redundant joints and could be a safety issue
+  moveit_msgs::msg::RobotTrajectory trajectory;
+  const double jump_threshold = 0.0;
+  const double eef_step = 0.01;
+  double fraction = move_group.computeCartesianPath(waypoints, eef_step, jump_threshold, trajectory);
+  RCLCPP_INFO(logger, "Visualizing plan 4 (Cartesian path) (%.2f%% achieved)", fraction * 100.0);
+
+  move_group.execute(trajectory);
+
+  // Next step goes here
+
+  // Shutdown ROS
+  rclcpp::shutdown();
+  return 0;
+}
